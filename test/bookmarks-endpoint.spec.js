10d8b2c3c58889b56562bbde930bcc83d956371a
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-14 20:31:00 -0700

    call befor each for insert bookmarks

--- test/bookmarks-endpoint.spec.js
+++ test/bookmarks-endpoint.spec.js
@@ -1,3 +1,29 @@
 const knex = require('knex')
 const fixtures = require('./bookmarks-fixtures')
 const app = require('../src/app')
+
+describe('Bookmarks Endpoints', () => {
+    let db
+  
+    before('make knex instance', () => {
+      db = knex({
+        client: 'pg',
+        connection: process.env.TEST_DB_URL,
+      })
+      app.set('db', db)
+    })
+  
+    after('disconnect from db', () => db.destroy())
+  
+    before('cleanup', () => db('bookmarks').truncate())
+  
+    afterEach('cleanup', () => db('bookmarks').truncate())
+  
+    describe(`Unauthorized requests`, () => {
+      const testBookmarks = fixtures.makeBookmarksArray()
+  
+      beforeEach('insert bookmarks', () => {
+        return db
+          .into('bookmarks')
+          .insert(testBookmarks)
+      })
