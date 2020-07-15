b65c2bec846900caa4b58d5909fb13d094d9ef67
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-14 20:40:09 -0700

    build bookmark fixtures js

---
+++ test/bookmarks-fixtures.js
@@ -0,0 +1,9 @@
+function makeBookmarksArrayfunction makeBookmarksArray() {
+    return [
+      {
+        id: 1,
+        title: 'Thinkful',
+        url: 'https://www.thinkful.com',
+        description: 'Think outside the classroom',
+        rating: 5,
+      },
