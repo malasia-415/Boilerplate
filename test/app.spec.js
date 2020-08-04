5a91f7d4aa6a399332e15a35bb56dae00ecec2f4
Malasia Woods <malasiawoods@Malasias-Air.hsd1.ca.comcast.net>
2020-08-03 18:07:04 -0700

    building specs bookmarks

--- test/app.spec.js
+++ test/app.spec.js
@@ -26,3 +26,36 @@ describe('Bookmarks Endpoints', () => {
         .send({ title: 'test-title', url: 'http://some.thing.com', rating: 1 })
         .expect(401, { error: 'Unauthorized request' })
     })
+
+    it(`responds with 401 Unauthorized for GET /bookmarks/:id`, () => {
+      const secondBookmark = store.bookmarks[1]
+      return supertest(app)
+        .get(`/bookmarks/${secondBookmark.id}`)
+        .expect(401, { error: 'Unauthorized request' })
+    })
+
+    it(`responds with 401 Unauthorized for DELETE /bookmarks/:id`, () => {
+      const aBookmark = store.bookmarks[1]
+      return supertest(app)
+        .delete(`/bookmarks/${aBookmark.id}`)
+        .expect(401, { error: 'Unauthorized request' })
+    })
+  })
+
+  describe('GET /bookmarks', () => {
+    it('gets the bookmarks from the store', () => {
+      return supertest(app)
+        .get('/bookmarks')
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+        .expect(200, store.bookmarks)
+    })
+  })
+
+  describe('GET /bookmarks/:id', () => {
+    it('gets the bookmark by ID from the store', () => {
+      const secondBookmark = store.bookmarks[1]
+      return supertest(app)
+        .get(`/bookmarks/${secondBookmark.id}`)
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+        .expect(200, secondBookmark)
+    })
