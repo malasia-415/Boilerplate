27fbdba1f4b0ad5e903f97cc27bd147b43b9389d
Malasia Woods <malasiawoods@Malasias-Air.hsd1.ca.comcast.net>
2020-08-03 17:23:14 -0700

    edits building router

--- src/bookmarks-server/bookmarks-router.js
+++ src/bookmarks-server/bookmarks-router.js
@@ -20,3 +20,23 @@ bookmarksRouter
       }
     }
     const { title, url, description, rating } = req.body
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`)
+      return res.status(400).send(`'rating' must be a number between 0 and 5`)
+    }
+
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send(`'url' must be a valid URL`)
+    }
+
+    const bookmark = { id: uuid(), title, url, description, rating }
+
+    store.bookmarks.push(bookmark)
+
+    logger.info(`Bookmark with id ${bookmark.id} created`)
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark)
+  })
